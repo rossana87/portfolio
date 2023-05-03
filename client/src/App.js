e54c956c5fe4968f7dd999a746a22053f1add38a
Rossana87 <rossana.ventrella@gmail.com>
2023-05-03 16:13:18 +0100

    app page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,39 @@
 import { useEffect } from 'react'
 import axios from 'axios'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Components
+import Home from './components/Home'
+import AboutPage from './components/about/AboutPage'
+import NavBar from './components/common/NavBar'
+import ContactPage from './components/contact/ContactPage'
+import ProjectPage from './components/projects/ProjectPage'
 
 const App = () => {
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get('/api/products/') // * <-- replace with your endpoint
-      console.log(data)
-    }
-    getData()
-  })
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const { data } = await axios.get('/api/products/') // * <-- replace with your endpoint
+  //     console.log(data)
+  //   }
+  //   getData()
+  // })
 
-  return <h1>Hello World</h1>
+  return (
+
+    <div className='site-wrapper'>
+
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  )
 }
 
 export default App
