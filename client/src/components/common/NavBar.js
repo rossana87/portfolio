f5288cc0c518b96563d13b7ec6e65168d70d7abf
Rossana87 <rossana.ventrella@gmail.com>
2023-05-03 17:20:42 +0100

    navbar

--- client/src/components/common/NavBar.js
+++ client/src/components/common/NavBar.js
@@ -1,8 +1,27 @@
+import { Link } from 'react-router-dom'
 
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+import Logo from '../../images/logo.png'
 
 const NavBar = () => {
   return (
-    <h1>hello</h1>
+    <Navbar expand="md">
+      <Container>
+        <Navbar.Brand to="/" as={Link} className='logo'><img src={Logo} /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+          <Nav>
+            <Nav.Link to="/" as={Link}>Home</Nav.Link>
+            <Nav.Link to="/about" as={Link}>About</Nav.Link>
+            <Nav.Link to="/projects" as={Link}>Projects</Nav.Link>
+            <Nav.Link to="/contact" as={Link}>Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
