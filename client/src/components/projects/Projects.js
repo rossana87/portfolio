60447669f6f6d8c86079ca846f62d68613be0633
Rossana87 <rossana.ventrella@gmail.com>
2023-05-05 15:14:23 +0100

    projects page

--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -1,9 +1,26 @@
-
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 const Projects = () => {
   return (
     <section id='projects'>
       <h1>Projects</h1>
+      <Row xs={1} md={2} className="g-4">
+        <Col>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </section>
   )
 }
