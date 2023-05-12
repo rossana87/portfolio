632fa484e4c8161b0bc8502260ec20b9c3e6e2b9
Rossana87 <rossana.ventrella@gmail.com>
2023-05-12 10:21:30 +0100

    small change

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -18,7 +18,7 @@ const About = () => {
                 Hi 👋! My name is Rossana, I&apos;m Italian and I have been living in London for the last ten years. Before moving to London I travelled all over the world, which instilled in me a lifelong <span className='about-hobby'>love of languages</span>, I speak Italian (obviously), English, and Russian.
                 <br />
                 <br />
-                During my university years, I was introduced to coding, and while I loved using HTML and CSS, I felt limited by the language&apos;s constraints. This motivated me to broaden my knowledge to create more engaging websites, which led me to enrol in an immersive Software Engineering Course at General Assembly. This experience allowed me to make a complete career shift and pursue development as a profession.
+                During my university years, I was introduced to coding, and while I loved using HTML and CSS, I felt limited by the language&apos;s constraints. This motivated me to broaden my knowledge to create more engaging websites, which led me to enrol in an immersive Software Engineering Course at General Assembly. This experience is allowing me to make a complete career shift and pursue development as a profession.
                 <br />
                 <br />
                 In my free time, you&apos;ll find me practising <span className='about-hobby'>Calisthenics</span>. This is definitely my medicine for keeping myself healthy and happy. Committing to Calisthenics has helped me to build habits and consistency. During evenings, I enjoy spending my time <span className='about-hobby'>knitting</span> while relaxing on the sofa or perhaps even unravelling an old project.
